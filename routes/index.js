2a7fc92ebf88539ee06af8a49715b7502eb6924a
Reira <731634265@qq.com>
2016-11-17 22:15:58 +0800

    list is ok

--- routes/index.js
+++ routes/index.js
@@ -11,27 +11,35 @@ router.get('/test_index', function(req, res, next) {
 });
 
 router.get('/list', function(req, res, next) {
-  var category = req.query.category;
-  var sort_Type = "rank";
-  var up_down = 1;
-  if(req.query.sort==1) {
-    sort_Type = "total_rating";
-    up_down = -1;
+  var categorystring = ""+req.query.category;
+  var sort_Type = {"rank":1};
+  if(req.query.sort=='1') {
+    sort_Type = {"total_rating":-1};
   }
-  if(req.query.sort==2) {
-    sort_Type = "average_rating";
-    up_down = -1;
+  if(req.query.sort=='2') {
+    sort_Type = {"install_estimated":-1};
   }
-  var start_point = req.query.start;
+  var category = {};
+  if(categorystring != ""){
+    categorystring = categorystring.replace(/_/g,' ');
+    category = {"category":categorystring};
+  }
+  var start_point = parseInt(req.query.start,10);
 
   var findDocuments = function (db) {
     var collection = db.collection('maap');
-    collection.find().sort( { sort_Type :up_down}).skip(start_point-1).limit(10).toArray(function (err, docs) {
+    var counts = collection.find(category).count();
+    console.log(sort_Type);
+    console.log(category);
+    collection.find(category).sort(sort_Type).skip(start_point-1).limit(20).toArray(function (err, docs) {
       if (!err) {
         console.log("Found the following records");
+        console.log(counts);
         res.render('list', {
           title: 'list',
-          sort: sort_Type,
+          total: counts,
+          cate: categorystring,
+          sort: req.query.sort,
           start: start_point,
           list: docs,
         });
