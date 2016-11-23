d116be36986bfbd7897852209099543564ad2fdc
Reira <731634265@qq.com>
2016-11-23 16:01:12 +0800

    version 0.8

--- routes/index.js
+++ routes/index.js
@@ -36,7 +36,59 @@ router.get('/test_index', function(req, res, next) {
 });
 
 router.get('/list', function(req, res, next) {
+  var categorystring = ""+req.query.category;
+  var sort_Type = {"rank":1};
+  if(req.query.sort=='1') {
+    sort_Type = {"total_rating":-1};
+  }
+  if(req.query.sort=='2') {
+    sort_Type = {"installs_estimated":-1};
+  }
+  var category = {};
+  if(categorystring != ""){
+    categorystring = categorystring.replace(/_/g,' ');
+    category = {"category":categorystring};
+  }
+  var start_point = parseInt(req.query.start,10);
 
+  var findDocuments = function (db) {
+    var collection = db.collection('maap');
+    console.log(collection.find(category).count());
+    var counts = "";
+    var result = collection.find(category).count(function(err,docs){
+      if(!err){
+        counts = docs;
+        console.log(counts);
+      }
+    });
+
+    collection.find(category).sort(sort_Type).skip(start_point-1).limit(20).toArray(function (err, docs) {
+      if (!err) {
+        console.log(sort_Type);
+        console.log(category);
+        console.log(counts);
+        console.log("Found the following records");
+        res.render('list', {
+          title: 'list',
+          counts: counts,
+          cate: categorystring,
+          sort: req.query.sort,
+          start: start_point,
+          list: docs,
+        });
+      }
+      else {
+        console.log("Not Found");
+        return;
+      }
+    });
+  }
+  // Use connect method to connect to the server
+  MongoClient.connect(url, function (err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
+    findDocuments(db);
+  });
 });
 
 router.get('/LibRadar', function(req, res, next) {
